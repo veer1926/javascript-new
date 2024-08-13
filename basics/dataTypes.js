909432dbfa5621c712eb71afcb19db1445d6f1bc
veer1926 <129360683+veer1926@users.noreply.github.com>
2024-08-13 18:20:09 +0000

    comit done

---
+++ basics/dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version 
+
+console.log(3*4)
+
+console.log('hello world') 
+
+let name = 'hello'
+
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// num => standalone value
+// undefined => 
+// symbol => unique 
+
+// object are also 
---
+++ basics/variables.js
@@ -0,0 +1,5 @@
+const account = '23421'
+let name = 'mike'
+let accountNo;
+
+console.log(account,accountNo,)
